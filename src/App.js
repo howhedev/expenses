de077d74d59ae4d36c25f9c186fe6d9872adb1ce
howhedev <pet.kolencik@gmail.com>
2022-10-13 11:40:17 +0200

    test

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
   });
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="app" data-theme={theme}>
         <ThemeToggle theme={theme} onThemeChange={onThemeChange} />
         {mobileView && (
